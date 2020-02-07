d3ce8c848780f776269ce767731afef142346cd9
Mike Shultz <shultzm@gmail.com>
2020-02-06 20:54:50 -0700

    v0.1.3

--- ledgereth/__init__.py
+++ ledgereth/__init__.py
@@ -3,6 +3,6 @@ from ledgereth.objects import Transaction, SignedTransaction
 from ledgereth.accounts import find_account, get_account_by_path, get_accounts
 from ledgereth.transactions import sign_transaction, create_transaction
 
-__version__ = '0.1.2'
+__version__ = '0.1.3'
 __author__ = 'Mike Shultz'
 __email__ = 'mike@mikeshultz.com'
