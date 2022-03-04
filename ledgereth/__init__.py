d5782a36ebaa33b81230c52d3481bbd08ea78c5b
Mike Shultz <shultzm@gmail.com>
2022-03-04 00:36:25 -0700

    fix module exports and CLI call to create_transaction()

--- ledgereth/__init__.py
+++ ledgereth/__init__.py
@@ -1,12 +1,21 @@
 # flake8:noqa
 from ledgereth._meta import author, email, version
-from ledgereth.objects import Transaction, SignedTransaction
+from ledgereth.objects import (
+    Transaction,
+    Type1Transaction,
+    Type2Transaction,
+    SignedTransaction,
+    SignedType1Transaction,
+    SignedType2Transaction,
+)
 from ledgereth.accounts import find_account, get_account_by_path, get_accounts
 from ledgereth.transactions import sign_transaction, create_transaction
 
 __all__ = [
     "Transaction",
-    "SignedTransaction",
+    "Type1Transaction" "Type2Transaction" "SignedTransaction",
+    "SignedType1Transaction",
+    "SignedType2Transaction",
     "find_account",
     "get_account_by_path",
     "get_accounts",
