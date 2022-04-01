d109a94678058e27f2a0454f8754bbb2267ad1aa
Mike Shultz <shultzm@gmail.com>
2022-03-31 19:56:45 -0600

    use pytest.raises

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -1,4 +1,6 @@
 """ Test that exceptions are translated/rendered correctly """
+import pytest
+
 from ledgerblue.commException import CommException
 
 from ledgereth.comms import dongle_send
@@ -14,90 +16,73 @@ from ledgereth.exceptions import (
 
 def test_comms_exception(yield_dongle):
     with yield_dongle(exception=CommException("TEST", 0xFFFF, 0x00)) as dongle:
-        try:
+        with pytest.raises(LedgerError) as err:
             dongle_send(dongle, "GET_CONFIGURATION")
-            assert False
-        except Exception as err:
-            assert isinstance(err, LedgerError), f"Unexpected exception: {type(err)}"
+
+        assert "Unexpected error" in str(err.value)
 
 
 def test_comms_exception_invalid(yield_dongle):
     with yield_dongle(exception=CommException("TEST", 0x6A80, 0x00)) as dongle:
-        try:
+        with pytest.raises(LedgerInvalid) as err:
             dongle_send(dongle, "SIGN_TX_FIRST_DATA")
-            assert False
-        except Exception as err:
-            assert isinstance(err, LedgerInvalid), f"Unexpected exception: {type(err)}"
+
+        assert "Invalid data" in str(err.value)
 
 
 def test_comms_not_found(yield_dongle):
     with yield_dongle(exception=CommException("TEST", 0x6F00, 0x00)) as dongle:
-        try:
+        with pytest.raises(LedgerNotFound) as err:
             dongle_send(dongle, "GET_DEFAULT_ADDRESS_NO_CONFIRM")
-            assert False
-        except Exception as err:
-            assert isinstance(err, LedgerNotFound), f"Unexpected exception: {type(err)}"
+
+        assert "Unable to find Ledger" in str(err.value)
 
 
 def test_comms_exception_locked(yield_dongle):
     with yield_dongle(exception=CommException("TEST", 0x6B0C, 0x00)) as dongle:
-        try:
+        with pytest.raises(LedgerLocked) as err:
             dongle_send(dongle, "SIGN_TX_FIRST_DATA")
-            assert False
-        except Exception as err:
-            assert isinstance(err, LedgerLocked), f"Unexpected exception: {type(err)}"
+
+        assert "locked" in str(err.value)
 
 
 def test_comms_app_not_open(yield_dongle):
     with yield_dongle(exception=CommException("TEST", 0x6804, 0x00)) as dongle:
-        try:
+        with pytest.raises(LedgerAppNotOpened) as err:
             dongle_send(dongle, "SIGN_TX_FIRST_DATA")
-            assert False
-        except Exception as err:
-            assert isinstance(
-                err, LedgerAppNotOpened
-            ), f"Unexpected exception: {type(err)}"
+
+        assert "Ethereum app" in str(err.value)
 
     with yield_dongle(exception=CommException("TEST", 0x6D00, 0x00)) as dongle:
-        try:
+        with pytest.raises(LedgerAppNotOpened) as err:
             dongle_send(dongle, "SIGN_TX_FIRST_DATA")
-            assert False
-        except Exception as err:
-            assert isinstance(
-                err, LedgerAppNotOpened
-            ), f"Unexpected exception: {type(err)}"
+
+        assert "Ethereum app" in str(err.value)
 
     with yield_dongle(exception=CommException("TEST", 0x6D02, 0x00)) as dongle:
-        try:
+        with pytest.raises(LedgerAppNotOpened) as err:
             dongle_send(dongle, "SIGN_TX_FIRST_DATA")
-            assert False
-        except Exception as err:
-            assert isinstance(
-                err, LedgerAppNotOpened
-            ), f"Unexpected exception: {type(err)}"
+
+        assert "Ethereum app" in str(err.value)
 
 
 def test_comms_user_cancel(yield_dongle):
     with yield_dongle(exception=CommException("TEST", 0x6982, 0x00)) as dongle:
-        try:
+        with pytest.raises(LedgerCancel) as err:
             dongle_send(dongle, "SIGN_TX_FIRST_DATA")
-            assert False
-        except Exception as err:
-            assert isinstance(err, LedgerCancel), f"Unexpected exception: {type(err)}"
+
+        assert "cancelled" in str(err.value)
 
     with yield_dongle(exception=CommException("TEST", 0x6985, 0x00)) as dongle:
-        try:
+        with pytest.raises(LedgerCancel) as err:
             dongle_send(dongle, "SIGN_TX_FIRST_DATA")
-            assert False
-        except Exception as err:
-            assert isinstance(err, LedgerCancel), f"Unexpected exception: {type(err)}"
+
+        assert "cancelled" in str(err.value)
 
 
 def test_comms_unknown_error(yield_dongle):
     with yield_dongle(exception=CommException("TEST", 0xEEEE, 0x00)) as dongle:
-        try:
+        with pytest.raises(LedgerError) as err:
             dongle_send(dongle, "SIGN_TX_FIRST_DATA")
-            assert False
-        except Exception as err:
-            assert "UNKNOWN" in str(err)
-            assert isinstance(err, LedgerError), f"Unexpected exception: {type(err)}"
+
+        assert "UNKNOWN" in str(err.value)
