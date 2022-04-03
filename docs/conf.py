0ab7aadff3989e495dab7ab51a5ea27dec1d5bad
Mike Shultz <shultzm@gmail.com>
2022-04-02 19:50:57 -0600

    remove junk

--- docs/conf.py
+++ docs/conf.py
@@ -159,7 +159,7 @@ latex_documents = [
 
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
-man_pages = [(master_doc, "solidbyte", "ledgereth Documentation", [author], 1)]
+man_pages = [(master_doc, "ledgereth", "ledgereth Documentation", [author], 1)]
 
 
 # -- Options for Texinfo output ----------------------------------------------
