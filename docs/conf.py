77d3e7b344df062c592f3b1979a4f61f06639389
Mike Shultz <shultzm@gmail.com>
2023-11-15 21:11:01 -0700

    Bump version: 0.9.0 → 0.9.1

--- docs/conf.py
+++ docs/conf.py
@@ -25,9 +25,9 @@ copyright = "2022, Mike Shultz"
 author = "Mike Shultz"
 
 # The short X.Y version
-version = "0.9.0"
+version = "0.9.1"
 # The full version, including alpha/beta/rc tags
-release = "0.9.0"
+release = "0.9.1"
 
 
 # -- General configuration ---------------------------------------------------
